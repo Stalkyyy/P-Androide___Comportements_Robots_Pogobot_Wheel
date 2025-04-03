e4c08b48cb9e2ff2ad4125f34aa1a1ddb3cf499b
Stalkyyy <fernandes.enzo579@gmail.com>
2025-04-03 22:30:03 +0200

    First version - Safia

--- leader_following/main.c
+++ leader_following/main.c
@@ -1,82 +1,192 @@
-
-// Main include for pogobots, both for real robots and for simulations
 #include "pogobase.h"
+#include <stdlib.h>
+#include <time.h>
+
+// Prototypes des fonctions
+void detect_neighbours(void);
+void send_id_extremity(void);
+void election_leader(void);
+void set_leader_and_order(void);
+void send_id(uint16_t id);
+void send_position(uint16_t id, uint16_t ml, uint16_t mr);
+void random_walk_leader(void);
+void follow_leader(void);
+
+// struct message pour partager les ids des robots (notamment ceux des robots aux extrémités de la file indienne)
+typedef struct {
+    uint16_t id;
+} IDMSG;
+
+// struct du message envoyé par le leader aux autres pour les prévenir du leader élu (contient id du sender = leader)
+typedef struct {
+    uint16_t leader_id; // contenu du msg
+} LEADERMSG;
+
+// struct du message concernant position du leader à suivre
+typedef struct {
+    uint16_t id; // id du sender ici
+    uint16_t motorL;
+    uint16_t motorR;
+} POSITIONMSG;
 
 // "Global" variables should be inserted within the USERDATA struct.
-// /!\  In simulation, don't declare non-const global variables outside this struct, elsewise they will be shared among all agents (and this is not realistic).
 typedef struct {
-    // Put all global variables you want here.
     uint8_t data_foo[8];
     time_reference_t timer_it;
+    uint16_t my_id;
+    uint16_t leader_id;
+    bool is_leader;
+    uint16_t nb_neighbours; // normalement, il devrait y avoir max 2 voisins car disposition en file indienne
+    uint16_t predecessor_id; // id du robot à suivre dans la file indienne
 } USERDATA;
 
-// Call this macro in the same file (.h or .c) as the declaration of USERDATA
 DECLARE_USERDATA(USERDATA);
-
-// Don't forget to call this macro in the main .c file of your project (only once!)
 REGISTER_USERDATA(USERDATA);
-// Now, members of the USERDATA struct can be accessed through mydata->MEMBER. E.g. mydata->data_foo
-//  On real robots, the compiler will automatically optimize the code to access member variables as if they were true globals.
 
-
-// Init function. Called once at the beginning of the program (cf 'pogobot_start' call in main())
 void user_init(void) {
 #ifndef SIMULATOR
     printf("setup ok\n");
 #endif
 
-    // Init timer
     pogobot_stopwatch_reset(&mydata->timer_it);
-
-    // Set main loop frequency, message sending frequency, message processing frequency
-    main_loop_hz = 60;      // Call the 'user_step' function 60 times per second
+    main_loop_hz = 60;
     max_nb_processed_msg_per_tick = 0;
-    // Specify functions to send/transmit messages. See the "hanabi" example to see message sending/processing in action!
-    msg_rx_fn = NULL;       // If Null, no reception of message
-    msg_tx_fn = NULL;       // If Null, don't send any message
+    msg_rx_fn = NULL;
+    msg_tx_fn = NULL;
+    error_codes_led_idx = 3;
 
-    // Set led index to show error codes (e.g. time overflows)
-    error_codes_led_idx = 3; // Default value, negative values to disable
+    pogobot_infrared_set_power(3);
+    mydata->my_id = pogobot_helper_getid();
+    mydata->is_leader = false;
+    mydata->leader_id = -1;
+    mydata->predecessor_id = -1;
+    mydata->nb_neighbours = 0;
+
+    pogobot_led_setColor(120, 60, 0);
+
+    detect_neighbours();
+    send_id_extremity();
+    election_leader();
+    set_leader_and_order();
 }
 
+// Définition des fonctions (elles restent inchangées)
+void send_id(uint16_t id) {
+    IDMSG msg;
+    msg.id = id;
+    pogobot_infrared_sendLongMessage_omniSpe((uint8_t *)&msg, sizeof(msg));
+}
 
-// Step function. Called continuously at each step of the pogobot main loop
+void detect_neighbours(void) {
+    send_id(mydata->my_id);
+
+    pogobot_infrared_update();
+
+    while (pogobot_infrared_message_available()) {
+        message_t msg;
+        pogobot_infrared_recover_next_message(&msg);
+        IDMSG* received_msg = (IDMSG *)msg.payload;
+        mydata->nb_neighbours++;
+    }
+}
+
+void send_id_extremity(void) {
+    if (mydata->nb_neighbours == 0) {
+        send_id(mydata->my_id);
+    }
+}
+
+void election_leader(void) {
+    pogobot_infrared_update();
+    while (pogobot_infrared_message_available()) {
+        message_t msg;
+        pogobot_infrared_recover_next_message(&msg);
+        IDMSG* received_msg = (IDMSG *)msg.payload;
+
+        if (mydata->nb_neighbours == 1) {
+            if (mydata->my_id < received_msg->id) {
+                mydata->is_leader = true;
+                mydata->leader_id = mydata->my_id;
+                pogobot_led_setColor(0, 255, 0);
+                LEADERMSG leader_msg = { mydata->leader_id };
+                pogobot_infrared_sendLongMessage_omniSpe((uint8_t *)&leader_msg, sizeof(leader_msg));
+            }
+        }
+    }
+}
+
+void set_leader_and_order(void) {
+    pogobot_infrared_update();
+    while (pogobot_infrared_message_available()) {
+        message_t msg;
+        pogobot_infrared_recover_next_message(&msg);
+        LEADERMSG *leader_msg = (LEADERMSG *)msg.payload;
+
+        if (mydata->leader_id == -1) {
+            mydata->leader_id = leader_msg->leader_id;
+            mydata->predecessor_id = msg.header._sender_id;
+            pogobot_led_setColor(0, 0, 255);
+            pogobot_infrared_sendLongMessage_omniSpe((uint8_t *)leader_msg, sizeof(leader_msg));
+        }
+    }
+}
+
+void send_position(uint16_t id, uint16_t ml, uint16_t mr) {
+    POSITIONMSG msg = { id, ml, mr };
+    pogobot_infrared_sendLongMessage_omniSpe((uint8_t *)&msg, sizeof(msg));
+}
+
+void random_walk_leader(void) {
+    if (!mydata->leader_id) return;
+
+    int directions[] = { 0, 1, 3, 4 };
+    int random_index = rand() % 4;
+    int random_direction = directions[random_index];
+
+    uint16_t motorLeft = motorThreeQuarter;
+    uint16_t motorRight = motorThreeQuarter;
+
+    switch (random_direction) {
+        case 0: motorLeft += motorQuarter; motorRight += motorQuarter; break;
+        case 1: motorLeft += motorQuarter; motorRight -= motorHalf; break;
+        case 3: motorLeft -= motorHalf; motorRight += motorQuarter; break;
+        case 4: motorLeft = motorStop; motorRight = motorStop; break;
+    }
+
+    pogobot_motor_set(motorL, motorLeft);
+    pogobot_motor_set(motorR, motorRight);
+
+    send_position(mydata->my_id, motorLeft, motorRight);
+}
+
+void follow_leader(void) {
+    if (mydata->leader_id) return;
+
+    pogobot_infrared_update();
+
+    while (pogobot_infrared_message_available()) {
+        message_t msg;
+        pogobot_infrared_recover_next_message(&msg);
+        POSITIONMSG* received_msg = (POSITIONMSG *)msg.payload;
+
+        if (received_msg->id == mydata->predecessor_id) {
+            pogobot_motor_set(motorL, received_msg->motorL);
+            pogobot_motor_set(motorR, received_msg->motorR);
+            send_position(mydata->my_id, received_msg->motorL, received_msg->motorR);
+        }
+    }
+}
+
 void user_step(void) {
-    if (pogobot_ticks % 1000 == 0 && pogobot_helper_getid() == 0) {     // Only print messages for robot 0
-        printf(" HELLO WORLD !!!   Robot ID: %d   Current time: %lums  Timer: %luµs   pogobot_ticks: %lu\n",
-                pogobot_helper_getid(),
-                current_time_milliseconds(),
-                pogobot_stopwatch_get_elapsed_microseconds(&mydata->timer_it),
-                pogobot_ticks       // Increased by one at each execution of user_step
-                );
-    }
-
-    if ((uint32_t)(current_time_milliseconds() / 10000) % 2 == 0) {
-        pogobot_led_setColor(0,0,255);
-        pogobot_motor_set(motorL, motorFull);
-        pogobot_motor_set(motorR, motorStop);
-    } else {
-        pogobot_led_setColor(255,0,0);
-        pogobot_motor_set(motorL, motorStop);
-        pogobot_motor_set(motorR, motorFull);
-    }
-
-    mydata->data_foo[0] = 42;
+    random_walk_leader();
+    follow_leader();
 }
 
-
-// Entrypoint of the program
 int main(void) {
-    pogobot_init();     // Initialization routine for the robots
+    pogobot_init();
 #ifndef SIMULATOR
     printf("init ok\n");
 #endif
-
-    // Specify the user_init and user_step functions
     pogobot_start(user_init, user_step);
     return 0;
 }
-
-// MODELINE "{{{1
-// vim:expandtab:softtabstop=4:shiftwidth=4:fileencoding=utf-8
-// vim:foldmethod=marker
