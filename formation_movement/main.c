6c9843c75fb01f7c4e57fdfd2b55bd7945464adf
Melluchi <melanie.delluc@gmail.com>
2025-04-06 01:52:54 +0200

    pas opti mais pourquoi pas

--- formation_movement/main.c
+++ formation_movement/main.c
@@ -3,13 +3,17 @@
 
 #define INFRARED_POWER 3 // 1, 2, 3
 
+#define MAX_ROBOTS 5
+
 // "Global" variables should be inserted within the USERDATA struct.
 // /!\  In simulation, don't declare non-const global variables outside this struct, elsewise they will be shared among all agents (and this is not realistic).
 typedef struct {
     // Put all global variables you want here.
     uint8_t data_foo[8];
     time_reference_t timer_it;
-    int suitDeja;
+    uint8_t suitDeja; // indique si on suit déjà qlq (plus vraiment utile vu qu'on stocke les ids des robots mtn)
+    uint8_t id_robots_suivis[MAX_ROBOTS]; // la liste des ids des robots qu'on suit
+    uint8_t nb_robots_suivis; // le nombre des voisins qu'on suit
 } USERDATA;
 
 // Call this macro in the same file (.h or .c) as the declaration of USERDATA
@@ -43,47 +47,56 @@ void user_init(void) {
     pogobot_infrared_set_power(INFRARED_POWER);
     srand(pogobot_helper_getRandSeed());
 
-    mydata->suitDeja = 0; // au départ, le robot ne suit personne
+    mydata->suitDeja = 0; // au départ, le robot ne suit personne, c'est pour être sûr que l'id est vraiment l'id d'un robot et pas la valeur initiale
+    //mydata->id_robots_suivis = {0}; // id du robot qu'on suit 
+    mydata->nb_robots_suivis = 0;
 }
 
 
 // Step function. Called continuously at each step of the pogobot main loop
 void user_step(void) {
-    
+
     // Transmission d'un message
-    uint8_t msg_envoi[2];
-    msg_envoi[0] = 42;
-    msg_envoi[1] = mydata->suitDeja; // indique si le robot suit déjà qlq (pas d'inspi pour le nom j'avoue)
+    uint8_t msg_envoi[MAX_ROBOTS+2];
+    msg_envoi[0] = mydata->suitDeja; // indique si le robot suit déjà qlq (pas d'inspi pour le nom j'avoue)
+    msg_envoi[1] = mydata->nb_robots_suivis;
+    for(int i=0; i<MAX_ROBOTS; i++){
+        msg_envoi[i+2] = mydata->id_robots_suivis[i];
+    }
+    
     pogobot_infrared_sendLongMessage_omniSpe(msg_envoi, sizeof(msg_envoi));
 
     // Réception d'un message et décision du mouvement à faire
-    int move_id = -1;
+    uint8_t move_id = 5; // valeur de 5 car ça ne correspond à aucune IR direction
     pogobot_infrared_update();
 
+    uint8_t dir[4] = {0}; // compteur des senseurs des directions reçues
+    uint8_t id_robots_dir[4][MAX_ROBOTS] = {0}; // stockage des ids des robots détectés en fct des directions
+    
     while(pogobot_infrared_message_available()>=1){
         message_t msg;
         pogobot_infrared_recover_next_message(&msg);
 
         // si on détecte un robot
         if(msg.header._packet_type == ir_t_user){
-            //printf("Message reçu par le robot %d provenant du robot %d !\n", pogobot_helper_getid(), msg.header._sender_id);
+            uint8_t skip = 0;
 
-            // si le robot actuel ne suit personne mais que l'autre si, alors notre robot le suit
-            if(mydata->suitDeja < msg.payload[1]){
-                pogobot_led_setColor(255, 0, 0);
-                move_id = msg.header._receiver_ir_index; 
-                mydata->suitDeja = 1;
-            } else if(msg.payload[1] == mydata->suitDeja){ // si aucun ne suit déjà un robot (ou qu'ils suivent déjà tous deux un robot --> cas à traiter, possible pb, à voir mais là pas d'idées) 
-                if(pogobot_helper_getid() < msg.header._sender_id){ // le robot avec l'id le plus petit suit l'autre
-                    pogobot_led_setColor(255, 0, 0);
-                    move_id = msg.header._receiver_ir_index;  
-                    mydata->suitDeja = 1;
-                } else {
-                    pogobot_led_setColor(0, 255, 0);
+            // si c'est l'un des robots qui nous suit déjà alors on ne l'ignore
+            for(int i=0; i<msg.payload[1]; i++){
+                if(msg.payload[i+2] == pogobot_helper_getid()){
+                    skip = 1;
+                    break;
                 }
-            } else {
-                pogobot_led_setColor(0, 255, 0);
             }
+            if(skip){
+                continue;
+            }
+
+            // on stocke l'id du robot qui a envoyé la direction
+            id_robots_dir[msg.header._receiver_ir_index][dir[msg.header._receiver_ir_index]] = msg.header._sender_id;
+            
+            // on stocke le nombre de robots ayant envoyé cette direction
+            dir[msg.header._receiver_ir_index] ++;
         } 
         // si on détecte autre / un mur (à implémenter selon ce que va donner wall_allignment)
         else {
@@ -91,16 +104,62 @@ void user_step(void) {
         }
     }
 
+    // on regarde si le robot a vu des voisins qu'il peut suivre
+    uint8_t suitPas = 0;
+    for(int i=0; i<4; i++){
+        if(dir[i] > 0){
+            suitPas = 1;
+        }
+    }
+
+    // s'il a trouvé qlq à suivre, il s'adapte à la direction majoritaire
+    if(suitPas == 1){
+
+        // on cherche la direction où y a le plus de robots
+        uint8_t nb_robots_max = 0;
+        for(int i=0; i<4; i++){
+            if(nb_robots_max < dir[i]){
+                nb_robots_max = dir[i];
+            }
+        }
+
+        // on vérifie si y a une égalité entre plusieurs directions
+        uint8_t dir_egal[4] = {0};
+        uint8_t cpt_egalite = 0;
+        for(int i=0; i<4; i++){
+            if(nb_robots_max == dir[i]){
+                dir_egal[cpt_egalite] = i;
+                cpt_egalite++;
+            }
+        }
+
+        // si y a égalité, on prend l'une des directions au hasard
+        if(cpt_egalite > 1){
+            uint8_t idx = rand() % cpt_egalite;
+            move_id = dir_egal[idx];
+        } else {
+            move_id = dir_egal[0];
+        }
+
+        // on récupère les ids des voisins qu'on suit 
+        memcpy(mydata->id_robots_suivis, id_robots_dir[move_id], MAX_ROBOTS * sizeof(uint8_t));
+        mydata->suitDeja = 1; // plus vraiment utile ça je crois
+        mydata->nb_robots_suivis = dir[move_id];
+        pogobot_led_setColor(255, 0, 0);
+
+    } else { // s'il n'a vu personne d'intérêt, il continue sa vie
+        pogobot_led_setColor(0, 255, 0); 
+    }
+
+
     // Mouvement 
 
     // s'il le détecte à droite alors il va à droite 
     // et s'il le détecte en face il tourne un peu à droite pour éviter de le foncer dedans et ensuite le suivre
     if (move_id == 1 || move_id == 0) { 
-        //printf("droite\n");
         pogobot_motor_set(motorL, motorHalf);
         pogobot_motor_set(motorR, motorStop);
     } else if (move_id == 3) { // s'il le détecte à gauche alors il tourne à gauche
-        //printf("gauche\n");
         pogobot_motor_set(motorL, motorStop);
         pogobot_motor_set(motorR, motorHalf);
     } else { // si on a reçu le message de derrière on l'ignore, et idem si on n'a reçu aucun message
