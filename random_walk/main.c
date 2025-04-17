1be3c3960526dea07d6e1ea6bdad799e8e0bab9b
Enzo Pinho Fernandes <51473069+Stalkyyy@users.noreply.github.com>
2025-04-17 23:46:33 +0200

    Update main.c

--- random_walk/main.c
+++ random_walk/main.c
@@ -122,7 +122,7 @@ void move_left(void) {
         pogobot_motor_set(motorL, motorHalf);
         pogobot_motor_set(motorR, motorHalf);
 
-        pogobot_motor_dir_set(motorL, (mydata->dirLeft + 1 % 2));
+        pogobot_motor_dir_set(motorL, (mydata->dirLeft + 1) % 2);
         pogobot_motor_dir_set(motorR, mydata->dirRight);
     } else {
         pogobot_motor_set(motorL, motorStop);
@@ -138,7 +138,7 @@ void move_right(void) {
         pogobot_motor_set(motorR, motorHalf);
 
         pogobot_motor_dir_set(motorL, mydata->dirLeft);
-        pogobot_motor_dir_set(motorR, (mydata->dirRight + 1 % 2));
+        pogobot_motor_dir_set(motorR, (mydata->dirRight + 1) % 2);
     } else {
         pogobot_motor_set(motorL, motorHalf);
         pogobot_motor_set(motorR, motorStop);
