7c11c90f857810b5a0f61a637fd913a60c422f68
Melluchi <melanie.delluc@gmail.com>
2025-04-17 22:11:30 +0200

    à voir

--- wall_alignment/main.c
+++ wall_alignment/main.c
@@ -3,12 +3,20 @@
 
 #define INFRARED_POWER 3 // 1, 2, 3
 
+#define HAS_WHEEL false // Permet de choisir le cas où c'est un robot à roue, ou un robot à brosse.
+
 // "Global" variables should be inserted within the USERDATA struct.
 // /!\  In simulation, don't declare non-const global variables outside this struct, elsewise they will be shared among all agents (and this is not realistic).
 typedef struct {
     // Put all global variables you want here.
     uint8_t data_foo[8];
     time_reference_t timer_it;
+
+    uint16_t motorLeft;
+    uint8_t dirLeft;
+
+    uint16_t motorRight;
+    uint8_t dirRight;
 
     time_reference_t waiting_time;
 } USERDATA;
@@ -22,6 +30,56 @@ REGISTER_USERDATA(USERDATA);
 //  On real robots, the compiler will automatically optimize the code to access member variables as if they were true globals.
 
 
+// Si le robot a des roues, il ne change pas sa puissance, mais uniquement la direction du moteur.
+// Sinon (robot à brosse), il change la puissance des moteurs.
+
+
+void move_front(void) {
+    if (HAS_WHEEL) {
+        pogobot_motor_set(motorL, mydata->motorLeft);
+        pogobot_motor_set(motorR, mydata->motorRight);
+
+        pogobot_motor_dir_set(motorL, mydata->dirLeft);
+        pogobot_motor_dir_set(motorR, mydata->dirRight);
+    } else {
+        pogobot_motor_set(motorL, mydata->motorLeft);
+        pogobot_motor_set(motorR, mydata->motorRight);
+    }
+}
+
+void move_left(void) {
+    if (HAS_WHEEL) {
+        pogobot_motor_set(motorL, motorHalf);
+        pogobot_motor_set(motorR, motorHalf);
+
+        pogobot_motor_dir_set(motorL, (mydata->dirLeft + 1 % 2));
+        pogobot_motor_dir_set(motorR, mydata->dirRight);
+    } else {
+        pogobot_motor_set(motorL, motorStop);
+        pogobot_motor_set(motorR, motorHalf);
+    }
+}
+
+void move_right(void) {
+    if (HAS_WHEEL) {
+        pogobot_motor_set(motorL, motorHalf);
+        pogobot_motor_set(motorR, motorHalf);
+
+        pogobot_motor_dir_set(motorL, mydata->dirLeft);
+        pogobot_motor_dir_set(motorR, (mydata->dirRight + 1 % 2));
+    } else {
+        pogobot_motor_set(motorL, motorHalf);
+        pogobot_motor_set(motorR, motorStop);
+    }
+}
+
+void move_stop(void) {
+    pogobot_motor_set(motorL, motorStop);
+    pogobot_motor_set(motorR, motorStop);
+}
+
+
+
 // Init function. Called once at the beginning of the program (cf 'pogobot_start' call in main())
 void user_init(void) {
 #ifndef SIMULATOR
@@ -32,7 +90,7 @@ void user_init(void) {
     pogobot_stopwatch_reset(&mydata->timer_it);
 
     // Set main loop frequency, message sending frequency, message processing frequency
-    main_loop_hz = 60;      // Call the 'user_step' function 60 times per second
+    main_loop_hz = 30;      // Call the 'user_step' function 60 times per second
     max_nb_processed_msg_per_tick = 0;
     // Specify functions to send/transmit messages. See the "hanabi" example to see message sending/processing in action!
     msg_rx_fn = NULL;       // If Null, no reception of message
@@ -43,6 +101,24 @@ void user_init(void) {
 
     pogobot_infrared_set_power(INFRARED_POWER);
     srand(pogobot_helper_getRandSeed()); // Initialiser le générateur de nombres aléatoires
+
+    // Récupération des données de calibration des robots
+
+    uint16_t power_mem[3];
+    uint8_t dir_mem[3];
+
+    if(HAS_WHEEL){
+        pogobot_motor_power_mem_get(power_mem);
+        mydata->motorLeft = power_mem[1];
+        mydata->motorRight = power_mem[0];
+
+        pogobot_motor_dir_mem_get(dir_mem);
+        mydata->dirLeft = dir_mem[1];
+        mydata->dirRight = dir_mem[0];
+    } else {
+        mydata->motorLeft = motorHalf;
+        mydata->motorRight = motorHalf;
+    }
 
 }
 
@@ -61,15 +137,16 @@ void user_step(void) {
     // Réception d'un message
     uint8_t move_id = 5;
     pogobot_infrared_update();
-    int32_t end_waiting_time = 0; // le temps mis pour envoyer et recevoir une détection du mur
+    uint32_t end_waiting_time = 0; // le temps mis pour envoyer et recevoir une détection du mur
 
     while(pogobot_infrared_message_available() >= 1){
         message_t msg;
         pogobot_infrared_recover_next_message(&msg);
 
         // si mur détecté
-        if(msg.header._packet_type != ir_t_user){
+        if(msg.header._packet_type == ir_t_user){
             move_id = msg.header._receiver_ir_index;
+            //end_waiting_time += pogobot_stopwatch_get_elapsed_microseconds(&mydata->waiting_time);
             end_waiting_time += pogobot_stopwatch_get_elapsed_microseconds(&mydata->waiting_time);
         } else { // c'est un robot et qu'il se trouve devant nous
             if(msg.header._receiver_ir_index == 0 && msg.header._sender_ir_index == 2){
@@ -90,26 +167,19 @@ void user_step(void) {
     // si on a reçu la détection du mur rapidement (temps à revoir selon les tests)
     // alors on s'arrête devant le mur
     if(end_waiting_time <= 200) { // en microseconds
-        pogobot_motor_set(motorL, motorStop);
-        pogobot_motor_set(motorR, motorStop);
+        move_stop();
         pogobot_led_setColor(255, 0, 0);
-    } else if(end_waiting_time <= 500) {
-        pogobot_motor_set(motorL, motorHalf);
-        pogobot_motor_set(motorR, motorHalf);
-        pogobot_led_setColor(255, 255, 0);
     } else {
         pogobot_led_setColor(0, 255, 0);
         if (move_id == 1 || move_id == 2) { // au cas où le mur est derrière on commence à tourner
-            pogobot_motor_set(motorL, motorFull);
-            pogobot_motor_set(motorR, motorStop);
+            move_right();
         } else if (move_id == 3) {
-            pogobot_motor_set(motorL, motorStop);
-            pogobot_motor_set(motorR, motorFull);
+            move_left();
         } else { // si on a reçu le message d'en face on avance, et idem si on n'a reçu aucun message
-            pogobot_motor_set(motorL, motorFull);
-            pogobot_motor_set(motorR, motorFull);
+            move_front();
         }
     }
+
 }
 
 
